96b0c431def4d468f8503b8b9feafb826f4fa766
subhra khanda <84471896+Subhra46@users.noreply.github.com>
2024-07-04 22:35:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add your JavaScript logic here
+function navigateToCourse(courseSlug) {
+    // You can define logic to navigate to the specific course page
+    // For now, let's just log the selected course slug
+    console.log(`Navigating to course: ${courseSlug}`);
+}
